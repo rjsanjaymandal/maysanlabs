bdce8b6c3764f7cec4286a1800657251489198b4
rjsanjaymandal <rjsanjaymandal@gmail.com>
2026-05-07 00:23:37 +0530

    feat: implement ContactFooter component with call-to-action and site navigation links

--- src/components/ContactFooter.tsx
+++ src/components/ContactFooter.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { ArrowRight, Mail, MapPin, Twitter, Linkedin, Send, Instagram } from "lucide-react";
+import { ArrowRight, Mail, MapPin, Twitter, Linkedin, Send, Instagram, Facebook } from "lucide-react";
 
 const links = [
   { name: "Services", href: "/services" },
@@ -13,9 +13,10 @@ const links = [
 ];
 
 const socialLinks = [
+  { name: "Facebook", icon: Facebook, href: "https://www.facebook.com/maysanlabs" },
   { name: "Instagram", icon: Instagram, href: "https://www.instagram.com/maysanlabs" },
   { name: "LinkedIn", icon: Linkedin, href: "https://in.linkedin.com/company/maysanlabs" },
-  { name: "Twitter", icon: Twitter, href: "https://twitter.com" },
+  { name: "Twitter", icon: Twitter, href: "https://x.com/maysanlabs" },
 ];
 
 export default function ContactFooter() {
